3c0e380b451bd9355ac530093b7d72dbae944049
kafkalainen <joonas.hj.nivala@gmail.com>
2020-06-16 10:04:23 +0300

    Added ft_memdel.c and ft_putnbr.c, and checked the norm.

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jnivala <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/16 08:24:58 by jnivala           #+#    #+#             */
-/*   Updated: 2020/06/16 08:50:11 by jnivala          ###   ########.fr       */
+/*   Updated: 2020/06/16 10:03:06 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void		ft_putnbr(int n)
 		ft_putchar('-');
 		ft_putchar('2');
 		n = 147483648;
-	}	
+	}
 	if (n < 0)
 	{
 		ft_putchar('-');
