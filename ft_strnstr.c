5b712da573e4d3229b33d9714849f7ac9c6c976b
kafkalainen <joonas.hj.nivala@gmail.com>
2020-07-01 17:04:51 +0300

    Added strnstr.

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jnivala <jnivala@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 11:16:09 by jnivala           #+#    #+#             */
-/*   Updated: 2020/06/26 13:00:21 by jnivala          ###   ########.fr       */
+/*   Updated: 2020/07/01 00:35:05 by jnivala          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 char		*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t		i;
 	size_t		little_len;
 
-	i = 0;
+	little_len = 0;
 	if (ft_strcmp(little, "") == 0)
 		return ((char*)big);
 	little_len = ft_strlen(little);
-	while (big[i] != '\0' && i < len)
+	while (*big != '\0' && len)
 	{
-		if (big[i] == little[0])
+		if (ft_strncmp((char*)big, little, little_len) == 0)
 		{
-			if (ft_strncmp((char*)(big + i), little, little_len) == 0)
-				return (char*)(big + i);
+			if (len >= little_len)
+				return (char*)big;
+			else
+				return (NULL);
 		}
-		i++;
+		big++;
+		len--;
 	}
 	return (NULL);
 }
